30870e855e4772ad4d43d5ee1988c80fda31b7ef
AOx0 <50227494+AOx0@users.noreply.github.com>
2021-12-17 05:19:20 -0600

    Tests

--- tests/tests.rs
+++ tests/tests.rs
@@ -4,8 +4,20 @@ mod file_manager {
     use spcsv::file_manager::*;
 
     #[test]
-    fn count_lines_test() {
+    fn count_lines_test_1() {
         let f : File = File::new(&format!("{}/tests/sample-1.csv", current_dir().unwrap().display()));
         assert_eq!(1001, f.lines())
+    }
+
+    #[test]
+    fn count_lines_test_2() {
+        let f : File = File::new(&format!("{}/tests/sample-2.csv", current_dir().unwrap().display()));
+        assert_eq!(101, f.lines())
+    }
+
+    #[test]
+    fn count_lines_test_3() {
+        let f : File = File::new(&format!("{}/tests/sample-3.csv", current_dir().unwrap().display()));
+        assert_eq!(10001, f.lines())
     }
 }

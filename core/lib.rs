ee57d2915f16b8d36a21ba22957c5cbec3b78119
AOx0 <50227494+AOx0@users.noreply.github.com>
2021-12-21 01:04:38 -0600

    Removed mutex for non mutable values

--- core/lib.rs
+++ core/lib.rs
@@ -19,10 +19,10 @@ mod reader;
 
 #[derive(Clone)]
 struct Shared {
-    taget_dir: Arc<Mutex<String>>,
-    headers: Arc<Mutex<String>>,
+    taget_dir: Arc<String>,
+    headers: Arc<String>,
     reader: Arc<Mutex<BufReader>>,
-    file: Arc<Mutex<File>>
+    file: Arc<File>
 }
 
 pub fn app(args: Option<Args>) {
@@ -59,10 +59,10 @@ pub fn app(args: Option<Args>) {
     let buf_reader = Arc::new(Mutex::new(buf_reader));
 
     let shared = Sin::new(Shared {
-        taget_dir: Arc::new(Mutex::new(target_dir)),
-        headers:  Arc::new(Mutex::new(headers)),
+        taget_dir: Arc::new(target_dir),
+        headers:  Arc::new(headers),
         reader: buf_reader.clone(),
-        file:  Arc::new(Mutex::new(file))
+        file:  Arc::new(file)
     });
 
     for i in 1..=args.number_of_files {
@@ -70,17 +70,17 @@ pub fn app(args: Option<Args>) {
             let r = misc::read_n_lines(shared.reader.lock().unwrap().borrow_mut(), each);
             let mut f = std::fs::File::create( & format!(
                         "{}/{}_{}.csv",
-                        &shared.taget_dir.lock().unwrap(),
-                        shared.file.lock().unwrap().base_name().unwrap(),
+                        &shared.taget_dir,
+                        shared.file.base_name().unwrap(),
                         i
                     )).expect("Unable to create file");
 
-            f.write_all(shared.headers.lock().unwrap().as_bytes())
+            f.write_all(shared.headers.as_bytes())
                 .expect("Unable to write data");
             f.write_all(r.as_bytes()).expect("Unable to write data");
             if args.verbose {
                 println!(
-                    "Wrote {} rows to {}", each, &format!("{}/{}_{}.csv", &shared.taget_dir.lock().unwrap(), shared.file.lock().unwrap().base_name().unwrap(), i)
+                    "Wrote {} rows to {}", each, &format!("{}/{}_{}.csv", &shared.taget_dir, shared.file.base_name().unwrap(), i)
                 );
             }
 
@@ -94,8 +94,8 @@ pub fn app(args: Option<Args>) {
                             "Wrote {} remaining rows to {}", remain,
                             &format!(
                                 "{}/{}_{}.csv",
-                                &shared.taget_dir.lock().unwrap(),
-                                shared.file.lock().unwrap().base_name().unwrap(),
+                                &shared.taget_dir,
+                                shared.file.base_name().unwrap(),
                                 i
                             )
                         );
@@ -104,13 +104,13 @@ pub fn app(args: Option<Args>) {
                     let r = misc::read_n_lines(shared.reader.lock().unwrap().borrow_mut(), remain);
                     let mut f = std::fs::File::create(&format!(
                         "{}/{}_{}.csv",
-                        &shared.taget_dir.lock().unwrap(),
-                        shared.file.lock().unwrap().base_name().unwrap(),
+                        &shared.taget_dir,
+                        shared.file.base_name().unwrap(),
                         args.number_of_files + 1
                     ))
                         .expect("Unable to create file");
 
-                    f.write_all(shared.headers.lock().unwrap().as_bytes())
+                    f.write_all(shared.headers.as_bytes())
                         .expect("Unable to write data");
                     f.write_all(r.as_bytes()).expect("Unable to write data");
                     if args.verbose {
@@ -118,8 +118,8 @@ pub fn app(args: Option<Args>) {
                             "Wrote {} remaining rows to {}", remain,
                             &format!(
                                 "{}/{}_{}.csv",
-                                &shared.taget_dir.lock().unwrap(),
-                                shared.file.lock().unwrap().base_name().unwrap(),
+                                &shared.taget_dir,
+                                shared.file.base_name().unwrap(),
                                 args.number_of_files + 1
                             )
                         );
